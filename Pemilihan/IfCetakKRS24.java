6b9f075bcf8a1ad5d03f4367f16eea148ed94384
Rafael Dimas <rafadimas29@gmail.com>
2025-10-06 22:45:28 +0700

    Percobaan 1

---
+++ Pemilihan/IfCetakKRS24.java
@@ -0,0 +1,17 @@
+package Pemilihan;
+
+import java.util.Scanner;
+
+public class IfCetakKRS24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("---CETAK KRS---");
+        System.out.println("Apakah Anda sudah membayar UKT? (true/false)");
+        boolean isUKT = input.nextBoolean();
+        if (isUKT) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA!");
+        } 
+        
+    }
+}
