c88472a027293e5d0f09965d2dbc1761b232e5fe
Rafael Dimas <rafadimas29@gmail.com>
2025-10-07 16:09:45 +0700

    Modifikasi percobaan 2

---
+++ Pemilihan/IfElseCetakKRS24.java
@@ -0,0 +1,34 @@
+package Pemilihan;
+
+import java.util.Scanner;
+
+public class IfElseCetakKRS24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("---CETAK KRS---");
+        System.out.print("Masukkan Semester Saat Ini! ");
+        int semester = input.nextInt();
+        
+        if (semester == 1) {
+            System.out.println("KRS SEMESTER 1");
+        } else if (semester == 2) {
+            System.out.println("KRS SEMESTER 2");
+        } else if (semester == 3) {
+            System.out.println("KRS SEMESTER 3");
+        } else if (semester == 4) {
+            System.out.println("KRS SEMESTER 4");
+        } else if (semester == 5) {
+            System.out.println("KRS SEMESTER 5");
+        } else if (semester == 6) {
+            System.out.println("KRS SEMESTER 6");
+        } else if (semester == 7) {
+            System.out.println("KRS SEMESTER 7");
+        } else if (semester == 8) {
+            System.out.println("KRS SEMESTER 8");
+        } else {
+            System.out.println("Semester Tidak Valid");
+        }
+    }
+}
+           
+  
