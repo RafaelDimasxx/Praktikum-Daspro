2135e3722754d45c8fd431be8da1b1eee8555695
Rafael Dimas <rafadimas29@gmail.com>
2025-10-08 06:57:12 +0700

    Modifikasi percobaan 1

--- Pemilihan/IfCetakKRS24.java
+++ Pemilihan/IfCetakKRS24.java
@@ -11,7 +11,9 @@ public class IfCetakKRS24 {
         if (isUKT) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA!");
-        } 
+        } else {
+            System.out.println("Registrasi ditolak, bayar UKT terlebih dahulu!");
+        }
         
     }
 }
