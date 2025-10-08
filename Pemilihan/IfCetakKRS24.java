9b363ae9389df2a51435cba7235ef3ab923aa4e9
Rafael Dimas <rafadimas29@gmail.com>
2025-10-08 09:58:53 +0700

    Tugas 1

--- Pemilihan/IfCetakKRS24.java
+++ Pemilihan/IfCetakKRS24.java
@@ -8,12 +8,8 @@ public class IfCetakKRS24 {
         System.out.println("---CETAK KRS---");
         System.out.println("Apakah Anda sudah membayar UKT? (true/false)");
         boolean isUKT = input.nextBoolean();
-        if (isUKT) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA!");
-        } else {
-            System.out.println("Registrasi ditolak, bayar UKT terlebih dahulu!");
-        }
-        
+        String pesan = (isUKT) ? "Registrasi berhasil! Silahkan cetak KRS dan minta tanda tangan DPA!" : "Registrasi ditolak, bayar UKT terlebih dahulu!";
+        System.out.println(pesan);
+
     }
 }
