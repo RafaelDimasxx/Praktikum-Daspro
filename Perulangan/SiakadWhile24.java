c28dc0e9fe8d1c059fcccf4072ffb7b1892873a1
Rafael Dimas <rafadimas29@gmail.com>
2025-10-21 12:03:16 +0700

    Percobaan 1

---
+++ Perulangan/SiakadWhile24.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SiakadWhile24 {
+    public static void main(String[] args) {
+        double nilai;
+        int jmlMhsw, i = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan jumlah mahasiswa");
+        jmlMhsw = input.nextInt();
+        while (i < jmlMhsw) {
+            System.out.println("Masukkan nilai mahasiswa ke-" + (i+1));
+            nilai = input.nextDouble();
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai tidak valid, masukkan nilai lagi! ");
+                continue;
+            } 
+            if (nilai > 80 && nilai <=100) {
+                System.out.println("Nilai A");
+            } else if (nilai > 73 && nilai <=80) {
+                System.out.println("Nilai B+");
+            } else if (nilai > 65 && nilai <=73) {
+                System.out.println("Nilai B");
+            } else if (nilai > 60 && nilai <=65) {
+                System.out.println("Nilai C+");
+            } else if (nilai > 50 && nilai <=60) {
+                System.out.println("Nilai C");
+            } else if (nilai > 39 && nilai <=50) {
+                System.out.println("Nilai D");
+            } else {
+                System.out.println("Nilai E");
+            }
+            i++;
+        }
+    }
+}
