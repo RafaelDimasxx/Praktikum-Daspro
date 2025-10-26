265306fadc8d5275c78c13c69d56b808937b0a95
Rafael Dimas <rafadimas29@gmail.com>
2025-10-26 11:31:23 +0700

    Modifikasi percobaan 1

--- Perulangan/Siakadfor24.java
+++ Perulangan/Siakadfor24.java
@@ -4,6 +4,7 @@ public class Siakadfor24 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
         for(int i = 1; i <=10; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-" + i);
             nilai = input.nextDouble();
@@ -13,8 +14,15 @@ public class Siakadfor24 {
             } else if (nilai < terendah) {
                 terendah = nilai;
             }
+           if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Mahasiswa lulus: " + lulus);
+        System.out.println("Mahasiswa tidak lulus: " + tidakLulus);
     }
 }
