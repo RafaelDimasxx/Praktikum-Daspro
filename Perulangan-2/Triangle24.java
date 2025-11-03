3256b9e35a82b26acb16b1bdf6990f30de6a41c6
Rafael Dimas <rafadimas29@gmail.com>
2025-11-03 09:29:46 +0700

    Perbaiki triangle petanyaan 2

---
+++ Perulangan-2/Triangle24.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Triangle24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai N: ");
+        int n = input.nextInt();
+        int i = 0;
+        while (i <= n) {
+            int j = 0;
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            
+            i++;
+        }
+    }
+}
