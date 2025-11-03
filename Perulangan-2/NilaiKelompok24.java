1ef2894f853c7c85fb3c41d26475548676770e04
Rafael Dimas <rafadimas29@gmail.com>
2025-11-03 09:56:00 +0700

    Nilai Kelompok

---
+++ Perulangan-2/NilaiKelompok24.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NilaiKelompok24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int nilai;
+        float totalNilai, rataNilai;
+        for(int i = 1; i <= 6; i++) {
+            totalNilai = 0;
+            System.out.println("\nKelompok " + i);
+            for(int j = 1; j <=5; j++) {
+                System.out.print("Nilai dari kelompok penilai " + j + " : ");
+                nilai = input.nextInt();
+                totalNilai += nilai;
+            }
+            rataNilai = totalNilai / 5;
+            System.out.println("==== Rata-rata nilai kelompok " + i + " adalah " + rataNilai + " ====");
+        }
+    }
+}
