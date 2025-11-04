46727a045a36f15aa3f327413eac2c9058584bcf
Rafael Dimas <rafadimas29@gmail.com>
2025-11-04 14:32:18 +0700

    Modifikasi Percobaan 2

--- Array/ArrayNilai24.java
+++ Array/ArrayNilai24.java
@@ -3,13 +3,17 @@ public class ArrayNilai24 {
     public static void main(String[] args) {
         Scanner input =  new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.println("Masukkan nilai akhir ke- " + i + " : ");
             nilaiAkhir[i] = input.nextInt();
         }
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke- " + i + " : " + nilaiAkhir[i]);
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke- " + i + " lulus");
+            } else {
+                System.out.println("Mahasiswa ke- " + i + " tidak lulus");
+            }
         }
     }
 }
