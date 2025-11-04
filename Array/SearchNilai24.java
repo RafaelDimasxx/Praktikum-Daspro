9a1aa09d93d258c5083c2b1cfdd2928ca569009a
Rafael Dimas <rafadimas29@gmail.com>
2025-11-04 15:24:22 +0700

    Modifikasi 1 Percobaan 4

--- Array/SearchNilai24.java
+++ Array/SearchNilai24.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
+
 public class SearchNilai24 {
     public static void main(String[] args) {
-        int[] arrNilai = { 80, 85, 78, 96, 90, 82, 86 };
-        int key = 90;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan banyak nilai yang akan diinput! ");
+        int[] arrNilai = new int[input.nextInt()];
+        int key = 0;
         int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai ke-" + (i + 1) + " : ");
+            arrNilai[i] = input.nextInt();
+        }
+        System.out.print("Masukkan nilai yang ingin dicari! ");
+        key = input.nextInt();
+        for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i + 1;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai " + key + " ada di index ke-" + hasil);
+        System.out.println("Nilai " + key + " milik mahasiswa ke-" + hasil);
         System.out.println();
     }
 }
