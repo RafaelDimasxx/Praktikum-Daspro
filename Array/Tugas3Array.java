b08b88c7f6a237cf42687d8eb5da23e8b89a1a1d
Rafael Dimas <rafadimas29@gmail.com>
2025-11-14 18:45:22 +0700

    Tugas 3

--- Array/Tugas3Array.java
+++ Array/Tugas3Array.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 public class Tugas3Array {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] menu = {
+                "Nasi Goreng", "Mie Goreng", "Roti Bakar", "Kentang Goreng", "Teh Tarik", "Cappucino",
+                "Chocolate Ice" };
+        String makanan = "";
+        String makananFix = "";
+        String next = "";
+        do {
+            System.out.println("Mau pesan apa? ");
+            makanan = input.nextLine();
+            
+        for(int i = 0; i < menu.length; i++) {
+            if (makanan.equalsIgnoreCase(menu[i])) {
+                makananFix = menu[i];
+                break;
+            } else {
+                makananFix = "";
+            }
+        }
+        if (makananFix != "") {
+            System.out.println(makananFix + " ada."); 
+            
+        } else {
+            System.out.println("Menu tidak tersedia.");
+        }
         
-    }
+        System.out.println("Ada lagi?(y/n)");
+        next = input.nextLine();
+        } while (next.equalsIgnoreCase("y"));
+        System.out.println("===== SAMPAI JUMPA KEMBALI =====");
+        
+}
 }
