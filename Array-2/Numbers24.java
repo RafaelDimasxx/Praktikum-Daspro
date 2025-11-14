00ae41e267fc5b3d214a4f0fd2259edb5a556f3f
Rafael Dimas <rafadimas29@gmail.com>
2025-11-14 20:07:36 +0700

    Percobaan 3

---
+++ Array-2/Numbers24.java
@@ -0,0 +1,8 @@
+public class Numbers24 {
+    public static void main(String[] args) {
+        int [][] numbers = new int[3][];
+        numbers[0] = new int[5];
+        numbers[1] = new int[3];
+        numbers[2] = new int[1];
+    }
+}
