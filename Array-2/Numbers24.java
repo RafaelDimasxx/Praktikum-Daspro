10a8a4dc77ca07e1d55e07b9d7f96c9620e3c03f
Rafael Dimas <rafadimas29@gmail.com>
2025-11-14 20:12:41 +0700

    Pertanyaan 1

--- Array-2/Numbers24.java
+++ Array-2/Numbers24.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class Numbers24 {
     public static void main(String[] args) {
         int [][] numbers = new int[3][];
         numbers[0] = new int[5];
         numbers[1] = new int[3];
         numbers[2] = new int[1];
+
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.println(Arrays.toString(numbers[i]));
+        }
     }
 }
