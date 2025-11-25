b2bf0d20f320ea3b7ea53cfe478aa71183f14153
Rafael Dimas <rafadimas29@gmail.com>
2025-11-25 14:25:54 +0700

    Percobaan 1 Function

---
+++ Function/Kafe24.java
@@ -0,0 +1,16 @@
+public class Kafe24 {
+    public static void Menu() {
+        System.out.println("==== Menu Resto Kafe ====");
+        System.out.println("1. Kopi Hitam - Rp.15.000 ");
+        System.out.println("2. Cappuchino - Rp.20.000 ");
+        System.out.println("3. Latte - Rp.25.000 ");
+        System.out.println("4. Teh Tarik - Rp.15.000 ");
+        System.out.println("5. Roti Bakar - Rp.20.000 ");
+        System.out.println("6. Mie Goreng - Rp.25.000 ");
+        System.out.println("================================");
+        System.out.println("Silahkan pilih menu yang anda inginkan!");
+    }
+    public static void main(String[] args) {
+        Menu();
+    }
+}
