ed52c2705b6fe4f5f57b01c253a8f76e691bbeb2
Rafael Dimas <rafadimas29@gmail.com>
2025-11-30 22:41:34 +0700

    Percobaan 5 dengan fungsi

--- Function/HitungBalok24.java
+++ Function/HitungBalok24.java
@@ -12,11 +12,19 @@ public class HitungBalok24 {
         System.out.print("Masukkan tinggi: ");
         t = input.nextInt();
 
-        L = p * l;
+        L = hitungLuas(p, l);
         System.out.println("Luas persegi panjang adalah " + L);
-        vol = p * l * t;
+        vol = hitungVolume(t, p, l);
         System.out.println("Volume balok adalah " + vol);
 
         input.close();
+    }
+    static int hitungLuas(int pjg, int lb) {
+        int Luas = pjg * lb;
+        return Luas;
+    }
+    static int hitungVolume(int tinggi, int a, int b) {
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
     }
 }
